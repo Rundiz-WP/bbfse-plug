695ccf975627b6d748573067b3123e6451fb1f3a
ve3 <kawewong@gmail.com>
2026-02-07 12:11:15 +0700

    Remove calling to `load_plugin_textdomain()`.

--- App/App.php
+++ App/App.php
@@ -21,29 +21,12 @@ if (!class_exists('\\BBFSEPlug\\App\\App')) {
 
 
         /**
-         * Load text domain. (Language files)
-         * 
-         * @since 0.0.1
-         */
-        public function loadLanguage()
-        {
-            load_plugin_textdomain('bbfse-plug', false, dirname(plugin_basename(BBFSEPLUG_FILE)) . '/languages/');
-        }// loadLanguage
-
-
-        /**
          * Run the WP plugin app.
          * 
          * @since 0.0.1
          */
         public function run()
         {
-            add_action('init', function () {
-                // @link https://codex.wordpress.org/Function_Reference/load_plugin_textdomain Reference.
-                // load language of this plugin.
-                $this->loadLanguage();
-            });
-
             // class(es)/method(s) that must be called before auto register. ----------------------
             $StylesAndScripts = new Libraries\StylesAndScripts();
             $StylesAndScripts->manualRegisterHooks();
